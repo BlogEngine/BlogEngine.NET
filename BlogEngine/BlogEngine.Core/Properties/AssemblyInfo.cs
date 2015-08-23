64d79a8225f72fb02b79d2bd8b527ea18939d3bf
rxtur <rtur.net@gmail.com>
2015-08-23 13:29:23 -0500

    Rearranging things in Core

--- BlogEngine/BlogEngine.Core/Properties/AssemblyInfo.cs
+++ BlogEngine/BlogEngine.Core/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Security.Permissions;
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 [assembly: AllowPartiallyTrustedCallers]
-[assembly: AssemblyVersion("3.1.2.0")]
+[assembly: AssemblyVersion("3.1.2.1")]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
