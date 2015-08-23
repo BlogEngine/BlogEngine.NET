ea37f43c141881f24357f74d787520999dd2326b
rxtur <rtur.net@gmail.com>
2015-08-23 01:32:44 -0500

    Updates to Lte theme, added back mobile control for backwards compatibility

---
+++ BlogEngine/BlogEngine.NET/AppCode/Controls/MobileThemeSwitch.cs
@@ -0,0 +1,19 @@
+﻿namespace App_Code.Controls
+{
+    using System;
+    using System.Web.UI.WebControls;
+
+    /// <summary>
+    /// The <see cref="MobileThemeSwitch"/>
+    ///   no longer used, needed so older themes won't break
+    /// </summary>
+    public class MobileThemeSwitch : LinkButton
+    {
+        public MobileThemeSwitch() { }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+        }
+    }
+}
