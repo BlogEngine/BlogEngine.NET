951f217477263c3ee5abe883ffc25a23af631004
rxtur <rtur.net@gmail.com>
2015-09-01 14:47:13 -0500

    Fixing package references

--- BlogEngine/BlogEngine.Core/Properties/AssemblyInfo.cs
+++ BlogEngine/BlogEngine.Core/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Security.Permissions;
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 [assembly: AllowPartiallyTrustedCallers]
-[assembly: AssemblyVersion("3.1.2.6")]
+[assembly: AssemblyVersion("3.1.2.7")]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
