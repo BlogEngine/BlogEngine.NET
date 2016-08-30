0a222c97c422efeb25a033ac02d9ef5dbd53f683
Francis <farzin.seyfolahi@gmail.com>
2016-08-30 20:58:04 +0430

    New Standard theme implemented.

---
+++ BlogEngine/BlogEngine.NET/Custom/Themes/Standard/src/Gruntfile.js
@@ -0,0 +1,23 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'compact'
+                },
+                files: {
+                    'css/styles.css': 'scss/styles.scss',
+                }
+            }
+        },
+        watch: {
+            src: {
+                files: ['js/*.js', 'scss/**/*.scss'],
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
+};
---
+++ BlogEngine/BlogEngine.NET/Custom/Themes/Standard/src/js/custom.js
@@ -0,0 +1,40 @@
+﻿//
+var simpleCap = $("label[for=simpleCaptchaValue]").parent();
+simpleCap.hide();
+$("#commentCaptcha").append(simpleCap.html());
+
+//
+var aLogin = $(".item-login");
+var aLoginAttr = aLogin.attr("href");
+if (aLoginAttr == "/admin/") {
+    aLogin.removeClass("item-login");
+    aLogin.addClass("item-admin");
+}
+
+//
+if (location.pathname !== '/') {
+    $('.blog-nav ul li a[href*="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+} else {
+    $('.blog-nav ul li a[href="/"]').addClass('active');
+}
+
+//
+var postAdminLinks = $(".post-adminlinks a");
+if (postAdminLinks.length == 2) {
+    $(".post-adminlinks a:nth-child(1)").addClass("item-edit");
+    $(".post-adminlinks a:nth-child(2)").addClass("item-delete");
+}
+if (postAdminLinks.length == 4) {
+    $(".post-adminlinks a:nth-child(1)").addClass("hidden");
+    $(".post-adminlinks a:nth-child(2)").addClass("item-approve");
+    $(".post-adminlinks a:nth-child(3)").addClass("item-edit");
+    $(".post-adminlinks a:nth-child(4)").addClass("item-delete");
+}
+
+//
+if (!$.trim($('#commentlist').html()).length) {
+    $("#commentlist").parent().hide();
+}
+$("#btnSaveAjax").click(function () {
+    $("#commentlist").parent().show();
+});
