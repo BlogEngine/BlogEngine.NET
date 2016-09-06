2bcd219fb92c30d20dbbd9d1d81168e1f1b34792
rxtur <rtur.net@gmail.com>
2016-09-05 20:02:15 -0500

    Text widget not liking apostrophe (3.3.0.3)

--- BlogEngine/BlogEngine.Core/Properties/AssemblyInfo.cs
+++ BlogEngine/BlogEngine.Core/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Security.Permissions;
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 [assembly: AllowPartiallyTrustedCallers]
-[assembly: AssemblyVersion("3.3.0.2")]
+[assembly: AssemblyVersion("3.3.0.3")]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
