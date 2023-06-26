a85da490281e9687f63ffcb27b5db8dece89d2e2
Davi Gray <124840911+Davi-Gray@users.noreply.github.com>
2023-06-26 15:01:58 -0500

    Update packages, references, configurations to latest.

--- BlogEngine/BlogEngine.NET/App_GlobalResources/labels.designer.cs
+++ BlogEngine/BlogEngine.NET/App_GlobalResources/labels.designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option or rebuild the Visual Studio project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class labels {
@@ -5980,6 +5980,15 @@ namespace Resources {
         internal static string settingsUpdated {
             get {
                 return ResourceManager.GetString("settingsUpdated", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Share.
+        /// </summary>
+        internal static string share {
+            get {
+                return ResourceManager.GetString("share", resourceCulture);
             }
         }
         
